0e5380f1461a9f3998d01c3d92d29c79f5703b42
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2023-12-25 02:21:53 +0530

    Add files via upload

--- routes/blog.js
+++ routes/blog.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 const router = Router();
 
 const storage = multer.diskStorage({
@@ -31,10 +32,14 @@ router.get("/add-new", (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate("createdBy");
-  console.log("User", blog);
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+
   return res.render("blog", {
     user: req.user,
     blog,
+    comments,
   });
 });
 
@@ -54,4 +59,12 @@ router.post("/", upload.single("coverImage"), async (req, res) => {
   return res.redirect(`/blog/${blog._id}`);
 });
 
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
+});
 module.exports = router;
